99b8d2a86016185774cf373050e664460ba30700
henry <1151071+HenrYxZ@users.noreply.github.com>
2025-12-04 11:52:56 -0300

    wip: gltf loading 2

--- gltf.py
+++ gltf.py
@@ -1,9 +1,13 @@
-from pygltflib import GLTF2
+from pygltflib import GLTF2, Accessor, Skin, Node
 
 
 def main():
     filename = "assets/Fox.glb"
     gltf = GLTF2().load(filename)
+    current_scene = gltf.scenes[gltf.scene]
+    node_index = current_scene.nodes[0]
+    fox = gltf.nodes[node_index]
+    m = fox.matrix  # will output vertices for the box object
     print(gltf.accessors)
 
 
